70129850fba035322ae92bce630754147b36b194
FanJunqiao <151130065+FanJunqiao@users.noreply.github.com>
2023-11-26 16:13:57 +0800

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,5 +1,10 @@
-### train ###
+### inference with pretrain ###
 CUDA_VISIBLE_DEVICES=0 python main_mmDiff.py \
 --config mmDiff_config.yml --batch_size 512 \
---model_diff_path checkpoints/checkpoint.pth \
+--model_diff_path checkpoints/checkpoint.pth \ # add your pretrained model
+--doc test --exp exp --ni \
+
+### train ###
+CUDA_VISIBLE_DEVICES=0 python main_mmDiff.py --train \
+--config mmDiff_config.yml --batch_size 512 \
 --doc test --exp exp --ni \
